efc86d6962eb291b1bd32523bb8b5e558ad45b97
KimSungJun <jt2302@naver.com>
2026-09-18 10:50:11 +0900

    0014 미션 완료

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -5,6 +5,7 @@ import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.rsData.RsData;
+import com.back.shared.member.out.MemberApiClient;
 import com.back.shared.post.dto.PostDto;
 import com.back.boundedContext.shared.post.event.PostCreatedEvent;
 import com.back.global.eventPublisher.EventPublisher;
@@ -18,14 +19,14 @@ import java.util.Optional;
 public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
-    private final MemberFacade memberFacade;
+    private final MemberApiClient memberApiClient;
 
     public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
         eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
 
-        String randomSecureTip = memberFacade.getRandomSecureTip();
+        String randomSecureTip = memberApiClient.getRandomSecureTip();
 
         return new RsData<>(
                 "201-1",
