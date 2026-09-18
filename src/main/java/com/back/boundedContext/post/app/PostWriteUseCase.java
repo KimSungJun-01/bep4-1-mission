a3b80a8615c782aade8e5e1486c6fcc92d918ad1
KimSungJun <jt2302@naver.com>
2026-09-18 09:44:27 +0900

    0011 미션 완료

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -3,6 +3,7 @@ package com.back.boundedContext.post.app;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
+import com.back.global.rsData.RsData;
 import com.back.shared.post.dto.PostDto;
 import com.back.boundedContext.shared.post.event.PostCreatedEvent;
 import com.back.global.eventPublisher.EventPublisher;
@@ -17,11 +18,11 @@ public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
 
-    public Post write(Member author, String title, String content) {
+    public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
         eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
 
-        return post;
+        return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
     }
 }
